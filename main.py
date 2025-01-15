439489a12dcc35a834ec7ac2628a793371307ce7
Alexey Kozlovskiy <bylba4kaa@gmail.com>
2025-01-15 13:42:28 +0300

    upd

--- main.py
+++ main.py
@@ -112,37 +112,41 @@ def get_joined_groups(client) -> list[dict]:
 @client.on(events.NewMessage())
 async def handler(event: events.NewMessage.Event):
     global groups_info
+    if not groups_info:
+        return
     with open("keywords.json", "r", encoding="utf-8") as f:
         forward_chat_ids = json.loads(f.read())
 
     for obj in forward_chat_ids:
-        keyword = obj["keyword"]
-        chat_id = obj["id"]
-        group_title = obj["group_title"]
-        group_invite_link = obj["group_invite_link"]
+        keywords = obj.get("send", None)
+        group_link_or_title = obj.get("group", None)
 
-        if not chat_id:
-            if group_title:
-                chat_id = next((group["id"] for group in groups_info if group["title"] == group_title), None)
+        if not keywords:
+            print("Нет send")
+            return
+        if not group_link_or_title:
+            print("Нет group")
+            return
 
-            else:
-                if not "+" in group_invite_link:
-                    print(f"{group_invite_link} не является пригласительной ссылкой.")
-                group_title = get_title_group_from_link(group_invite_link)
-                chat_id = next((group["id"] for group in groups_info if group["title"] == group_title), None)
-            
-        if keyword.lower() in event.message.text.lower():
-            try:
-                await client.send_message(chat_id, event.message)
+        if "https://" in group_link_or_title:
+            group_title = get_title_group_from_link(group_link_or_title)
+        else:
+            group_title = group_link_or_title
 
-                print(f"Сообщение с ключевым словом '{keyword}' переслано в чат {chat_id}")
-            except Exception as ex:
-                print(f"Не удалось отправить сообщение в {chat_id}. Ошибка: {ex}")
+        chat_id = next((group["id"] for group in groups_info if group["title"].lower() == group_title.lower()), None)
+
+        for keyword in keywords:
+            if keyword.lower() in event.message.text.lower():
+                try:
+                    await client.send_message(chat_id, event.message)
+
+                    print(f"Сообщение с ключевым словом '{keyword}' переслано в чат {chat_id} ({group_link_or_title})")
+                except Exception as ex:
+                    print(f"Не удалось отправить сообщение в {chat_id} ({group_link_or_title}). Ошибка: {ex}")
 
 
-
+groups_info = None
 with client:
     groups_info = get_joined_groups(client)
     print("Юзербот запущен")
     client.run_until_disconnected()
-
