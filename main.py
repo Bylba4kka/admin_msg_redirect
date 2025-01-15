7cd8e3663578d6e784595ac11ec6c48d400d227d
Alexey Kozlovskiy <bylba4kaa@gmail.com>
2025-01-15 12:58:06 +0300

    upd

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import json
+import random
+import time
 from telethon import events
 from loader import PHONE_NUMBER, SOURCE, get_client, get_userinfo
+from bs4 import BeautifulSoup
+import requests
 
 
 F_NAME, API_ID, API_HASH = get_userinfo(PHONE_NUMBER)
@@ -8,40 +12,72 @@ client = get_client(PHONE_NUMBER, API_ID, API_HASH)
 
 
 
-@client.on(events.NewMessage(SOURCE))
+def get_title_group_from_link(invite_link):
+
+    r = requests.get(invite_link, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:133.0) Gecko/20100101 Firefox/133.0"})
+
+    soup = BeautifulSoup(r.text, "html.parser")
+
+    title = soup.find("div", class_="tgme_page_title").get_text(strip=True)
+    return title.lower()
+
+
+def get_joined_groups(client) -> list[dict]:
+    """
+    Получает присоединённые группы и возвращает список словарей, содержащих название группы, её username и ID.
+    """
+    groups_info = []
+    
+    # Перебор всех диалогов
+    for dialog in client.iter_dialogs():
+        try:
+            # Получаем название, username и ID группы
+            group_info = {
+                'title': dialog.name, # Название группы
+                'id': dialog.id  # ID группы
+            }
+            groups_info.append(group_info)
+        except AttributeError:
+            # Обработка ошибки, когда атрибуты отсутствуют
+            print(f"Ошибка: Для диалога {dialog.id} не найден необходимый атрибут.")
+    
+    return groups_info
+
+
+@client.on(events.NewMessage())
 async def handler(event: events.NewMessage.Event):
+    global groups_info
     with open("keywords.json", "r", encoding="utf-8") as f:
         forward_chat_ids = json.loads(f.read())
 
-    for keyword, chat_ids in forward_chat_ids.items():
-        
+    for obj in forward_chat_ids:
+        keyword = obj["keyword"]
+        chat_id = obj["id"]
+        group_title = obj["group_title"]
+        group_invite_link = obj["group_invite_link"]
+
+        if not chat_id:
+            if group_title:
+                chat_id = next((group["id"] for group in groups_info if group["title"] == group_title), None)
+
+            else:
+                group_title = get_title_group_from_link(group_invite_link)
+                print(group_title)
+                chat_id = next((group["id"] for group in groups_info if group["title"] == group_title), None)
+                print(chat_id)
+            
         if keyword.lower() in event.message.text.lower():
-            for chat_id in chat_ids:
-                with open("groups.json", "r", encoding="utf-8") as f:
-                    groups = json.loads(f.read())
-                    chat_id = groups.get(chat_id, chat_id)
-                    if isinstance(chat_id, dict):
-                        chat_id = chat_id["id"]
-                try:
-                    await client.send_message(chat_id, event.message)
-                    print(f"Сообщение с ключевым словом '{keyword}' переслано в чат {chat_id}")
-                except Exception as ex:
-                    print(f"Не удалось отправить сообщение в {chat_id}. Ошибка: {ex}")
+            try:
+                await client.send_message(chat_id, event.message)
+
+                print(f"Сообщение с ключевым словом '{keyword}' переслано в чат {chat_id}")
+            except Exception as ex:
+                print(f"Не удалось отправить сообщение в {chat_id}. Ошибка: {ex}")
+
 
 
 with client:
+    groups_info = get_joined_groups(client)
     print("Юзербот запущен")
     client.run_until_disconnected()
 
-    # {
-    # "#frontend": [-1002443692087],
-    # "#webdesign": [-1002443692087],
-    # "#cms": [-1002443692087, -4751053130],
-    # "#webdev": [-1002443692087, -4751053130],
-    # "#landing": -1002443692087,
-    # "#shop": [-1002443692087, -4751053130],
-    # "#backend": 4751053130,
-    # "": [-1002314677140],
-    # }
-
-    # 2307229106 -1002406205336
